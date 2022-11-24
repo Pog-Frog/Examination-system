c0abcfb554cf19a5812e85b5f76b3977d44f41b4
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2022-11-25 01:32:06 +0200

    initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+import axios from 'axios';
+
+document.getElementById("snap").addEventListener("click", function() {
+    axios({
+        method: 'post',
+        url: 'http://127.0.0.1:5656/plagiarism/predict',
+        headers: {}, 
+        data: {
+          'essays_dict' : {"1":"Hello World", "2":"Hello Worldsddds", "3":"hi world there"},
+            "cased": "False" // This is the body part
+        }
+      }).then(res => console.log(res.data));
+    context.drawImage(video, 0, 0, 640, 480);
+});
+
